b7589960540320ca85ce215a1c09c6285af2dbdf
Imanbekov Tologon <imanbekovtologon4@gmail.com>
2024-04-25 16:41:57 +0600

    Worked on delete logic 1. Added new IActionResult methods to MyTaskController.cs 2. Created delete html page Delete.cshtml

--- lesson55/Controllers/MyTaskController.cs
+++ lesson55/Controllers/MyTaskController.cs
@@ -1,5 +1,6 @@
 using lesson55.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace lesson55.Controllers;
 
@@ -37,8 +38,60 @@ public class MyTaskController : Controller
 
     public IActionResult MyTask(int id)
     {
-        if (id !=null)
-            return View(_context.Tasks.FirstOrDefault(t => t.Id == id));
+        return View(_context.Tasks.FirstOrDefault(t => t.Id == id));
+    }
+
+    public IActionResult Open(int? id)
+    {
+        if (id != null)
+        {
+            MyTask task = _context.Tasks.FirstOrDefault(t => t.Id == id);
+            if (task.Status == "Новая")
+            {
+                task.Status = "Открыта";
+                _context.SaveChanges();
+            }
+        }
         return RedirectToAction("Index");
+    }
+
+    public IActionResult Close(int? id)
+    {
+        if (id != null)
+        {
+            MyTask task = _context.Tasks.FirstOrDefault(t => t.Id == id);
+            if (task.Status=="Открыта")
+            {
+                task.Status = "Закрыта";
+                _context.SaveChanges();
+            }
+        }
+        return RedirectToAction("Index");
+    }
+    [HttpGet]
+    [ActionName("Delete")]
+    public async Task<IActionResult> ConfirmDelete(int? id)
+    {
+        if (id != null)
+        {
+            MyTask task = await _context.Tasks.FirstOrDefaultAsync(p => p.Id == id);
+            if (task != null)
+                if (task.Status=="Закрыта"|| task.Status=="Новая")
+                    return View(task);
+        }
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id != null)
+        {
+            MyTask task = new MyTask() { Id = id.Value };
+            _context.Entry(task).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        return NotFound();
     }
 }
